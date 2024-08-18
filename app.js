a19c689a935eda179c43c3f607cd14bf2dd11b4d
sonyerg <ericksonrguinto@gmail.com>
2024-08-18 16:45:22 +1200

    added router

--- app.js
+++ app.js
@@ -4,24 +4,12 @@ const bodyParser = require("body-parser");
 //Express js is all about middleware.
 const app = express();
 
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use("/add-product", (req, res, next) => {
-  res.send(
-    `<form action='/product' method='POST'>
-      <input type='text' name='title' />
-      <button type='submit'>Send</button>
-    </form>`
-  );
-});
-
-app.post("/product", (req, res, next) => {
-  console.log(req.body);
-  res.redirect("/");
-});
-
-app.use("/", (req, res, next) => {
-  res.send("<h1>Hello from express</h1>");
-});
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
