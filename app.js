d2cb937077a18e12bd2b760664048ae0526e1449
sonyerg <ericksonrguinto@gmail.com>
2024-08-18 16:11:10 +1200

    working with express js middleware

--- app.js
+++ app.js
@@ -1,7 +1,20 @@
-const http = require("http");
-
 const express = require("express");
 
-const server = http.createServer();
+const app = express();
 
-server.listen(3000);
+//Express js is all about middleware.
+
+app.use("/", (req, res, next) => {
+  console.log("This is middleware");
+  next(); //allows request to continue to the next middleware in line
+});
+
+app.use("/users", (req, res, next) => {
+  res.send("<h1>Users page</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  res.send("<h1>Hello from express</h1>");
+});
+
+app.listen(3000);
