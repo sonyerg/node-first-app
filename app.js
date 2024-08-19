e71a0b31fc4505e8c0478fda0418dece80702d99
sonyerg <ericksonrguinto@gmail.com>
2024-08-19 19:58:32 +1200

    sharing data across req and users

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const bodyParser = require("body-parser");
 //Express js is all about middleware.
 const app = express();
 
-const adminRoutes = require("./routes/admin");
+const adminData = require("./routes/admin");
 const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 //redirect request for files to public folder
 app.use(express.static(path.join(__dirname, "public")));
 
-app.use("/admin", adminRoutes);
+app.use("/admin", adminData.router);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
