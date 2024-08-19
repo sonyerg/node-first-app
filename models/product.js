2d58b39f964ca544a3bd100ffc92c5f2400a93c9
sonyerg <ericksonrguinto@gmail.com>
2024-08-20 10:21:50 +1200

    store data in files via model

--- models/product.js
+++ models/product.js
@@ -1,4 +1,7 @@
-const products = [];
+const fs = require("fs");
+const path = require("path");
+
+const rootDir = require("../utils/path");
 
 module.exports = class Product {
   constructor(t) {
@@ -6,10 +9,29 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    const p = path.join(rootDir, "data", "products.json");
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      } else {
+        console.log("error file content", err);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), (err) => {
+        console.log(err);
+      });
+    });
   }
 
   static fetchAll() {
-    return products;
+    const p = path.join(rootDir, "data", "products.json");
+
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        return [];
+      }
+      return JSON.parse(fileContent);
+    });
   }
 };
