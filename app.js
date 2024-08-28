b7394cde501f19807a482d55b857d69febf8ea9e
sonyerg <ericksonrguinto@gmail.com>
2024-08-29 09:23:02 +1200

    added dummy user check

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const app = express();
 
 const errorController = require("./controllers/error");
 const sequelize = require("./utils/datbase");
+const Product = require("./models/product");
+const User = require("./models/user");
 
 // app.set("view engine", "pug");
 app.set("view engine", "ejs");
@@ -19,16 +21,38 @@ app.use(bodyParser.urlencoded({ extended: false }));
 //redirect request for files to public folder
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
+
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
+User.hasMany(Product);
+
 //syncs your model to the database by creating the appropriate tables and relations
 sequelize
   .sync()
   .then((result) => {
-    // console.log(result);
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: "Erickson", email: "erick@test.com" });
+    } else {
+      return user;
+    }
+  })
+  .then((user) => {
+    console.log("Username:", user.name, "User Email:", user.email);
     app.listen(3000);
   })
   .catch((err) => {
