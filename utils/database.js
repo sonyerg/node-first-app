b4f74ca4c64068bdd6e16a3f7065f8e07be8c72c
sonyerg <ericksonrguinto@gmail.com>
2024-09-02 22:28:25 +1200

    added connection to mongodb

--- utils/database.js
+++ utils/database.js
@@ -1,8 +1,30 @@
-const { Sequelize } = require("sequelize");
+const mongodb = require("mongodb");
+const MongoClient = mongodb.MongoClient;
+require("dotenv").config();
 
-const sequelize = new Sequelize("node-complete", "root", "Secretmysql!1909", {
-  dialect: "mysql",
-  host: "localhost",
-});
+const uri = process.env.MONGODB_URI;
 
-module.exports = sequelize;
+let _db;
+
+const mongoConnect = (callback) => {
+  MongoClient.connect(uri)
+    .then((client) => {
+      console.log("Connected to MongoDB");
+      _db = client.db();
+      callback();
+    })
+    .catch((err) => {
+      console.log(err);
+      throw err;
+    });
+};
+
+const getDb = () => {
+  if (_db) {
+    return _db;
+  }
+  throw "No database found!";
+};
+
+exports.mongoConnect = mongoConnect;
+exports.getDb = getDb;
