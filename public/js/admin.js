5bac7dc357e8026b69c39b9d89f6504258a34072
sonyerg <ericksonrguinto@gmail.com>
2025-02-09 20:39:57 +1300

    corrected missing character

--- public/js/admin.js
+++ public/js/admin.js
@@ -1,6 +1,6 @@
 const deleteProduct = (btn) => {
   const productId = btn.parentNode.querySelector("[name=productId]").value;
-  const csrfToken = btn.parentNode.querySelector("[name=_csrf").value;
+  const csrfToken = btn.parentNode.querySelector("[name=_csrf]").value;
 
   const productElement = btn.closest("article");
 
